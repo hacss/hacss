8e3bfce14f5a6955f3417a2573daaee0e08dfeaa
Nick Saunders <nick@saunde.rs>
2020-01-19 13:09:55 -0700

    Remove unnecessary config bits from example.

--- example/config.js
+++ example/config.js
@@ -1,10 +1,7 @@
-const config = require("../config/index.js"); // When installed via NPM, require("hacss/config") instead.
 const { color, mapArgs } = require("../config/utils.js");
 
 module.exports = {
-  ...config,
   rules: {
-    ...config.rules,
     Tstk: mapArgs((size, color) => `
       text-shadow:
         -${size} -${size} 0 ${color},
