5268c90c6f2050a082d279af6666de031d3c7635
Nick Saunders <nick@saunde.rs>
2020-03-06 12:07:05 -0700

    Merge Web site into main Hacss project.

---
+++ site.js
@@ -0,0 +1,40 @@
+const hacss = require("./index.js");
+const { customConfig } = require("./config/index.js");
+const testConfig = require("./test/config.js");
+import example from "./test/index.html";
+
+(function() {
+  const style = document.createElement("style");
+  document.head.appendChild(style);
+
+  const container = document.createElement("div");
+  container.className = "Pos(a) T(0) End(0) B(0) Start(0) D(f) Fxf(r) Fxf(c)--sm Fxf(c)--md";
+  document.body.insertBefore(container, document.body.firstChild);
+
+  const editPanel = document.createElement("div");
+  editPanel.className = "Fxg(1) Fxs(1) Fxb(1/2)";
+  container.appendChild(editPanel);
+
+  const previewPanel = document.createElement("div");
+  previewPanel.className = "Fxg(1) Fxs(1) Fxb(1/2) Ov(a) Pos(r)";
+  updatePreview(example);
+  container.appendChild(previewPanel);
+
+  const editor = ace.edit(editPanel);
+  editor.setTheme("ace/theme/cobalt");
+  editor.session.setMode("ace/mode/html");
+  editor.session.setOptions({
+    tabSize: 2,
+    useSoftTabs: true,
+  });
+  editor.session.setValue(example);
+  editor.session.on("change", function() {
+    var code = editor.session.getValue();
+    updatePreview(code);
+  });
+
+  function updatePreview(code) {
+    style.textContent = hacss(code, customConfig(testConfig));
+    previewPanel.innerHTML = code;
+  }
+})();
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
+const HtmlIncludeAssetsPlugin = require("html-webpack-include-assets-plugin");
+
+module.exports = {
+  entry: ["./output.js", "./site.js"],
+  output: {
+    path: path.join(__dirname, "site"),
+    filename: "app.js",
+  },
+  mode: "production",
+  plugins: [
+    new CopyPlugin([
+      { from: "node_modules/ace-builds/src-min/**/*", to: "ace", flatten: true },
+      { from: "node_modules/prettier/standalone.js", to: "prettier" },
+      { from: "node_modules/prettier/parser-postcss.js", to: "prettier" },
+    ]),
+    new HtmlPlugin({ title: "Hacss Playground" }),
+    new HtmlIncludeAssetsPlugin({
+      assets: [
+        "ace/ace.js",
+        "prettier/standalone.js",
+        "prettier/parser-postcss.js",
+      ],
+      append: false,
+    }),
+  ],
+  externals: {
+    "prettier/standalone": "prettier",
+    "prettier/parser-postcss": "prettierPlugins.postcss",
+  },
+  module: {
+    rules: [
+      {
+        test: /output\.js$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "val-loader",
+            options: {
+              sources: "./site.js",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["raw-loader"],
+      },
+      {
+        test: /\/test\/index\.html$/,
+        use: [
+          {
+            loader: "string-replace-loader",
+            options: {
+              search: `<link rel="stylesheet" href="styles.css" />`,
+              replace: "",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
