3b6e14d2c7b27322512c42237f2c0634c33aebba
Nick Saunders <nick@saunde.rs>
2020-04-29 16:04:17 -0700

    Update test to remove plugin dependency.

--- test/config.js
+++ test/config.js
@@ -1,22 +1,15 @@
-const globalVariables = require("../plugins/global-variables.js");
-const indexedVariables = require("../plugins/indexed-variables.js");
-
 module.exports = {
   mediaQueries: {
     "medium": "only screen and (min-width: 600px) and (max-width: 1199px)",
     "large": "only screen and (min-width: 1200px)",
   },
   plugins: [
-    globalVariables({
-      "sans-serif": "'Inter', sans-serif",
-    }),
-    indexedVariables({
-      "font-size": {
-        medium: "16px",
-        "medium-plus": "20px",
-        large: "28px",
-        "extra-large": "40px",
-      },
-    }),
+    decls => {
+      if ("font-family" in decls) {
+        decls["font-family"] =
+          decls["font-family"].replace("sans-serif", "'Inter', sans-serif");
+      }
+      return decls;
+    },
   ],
 };
